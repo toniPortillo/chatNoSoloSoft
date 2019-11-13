f6e3af20a9d66bf643ea26a1b1e017688ad02ec4
toniPortillo <toniplsec@gmail.com>
2019-11-13 09:26:09 +0100

    Cambio menor en el Footer para poner enlace al repositorio del proyecto

--- frontend2/src/components/Foot/Foot.vue
+++ frontend2/src/components/Foot/Foot.vue
@@ -1,7 +1,7 @@
 <template>
     <footer>
         <hr/>
-        <p class="foot">Construido por <a href="https://github.com/toniPortillo/chatNoSoloSoft">Toni Portillo</a></p>
+        <p class="foot">Construido por <a href="https://github.com/toniPortillo/chatNoSoloSoft">Toni Portillo </a></p>
     </footer>
 </template>
 
